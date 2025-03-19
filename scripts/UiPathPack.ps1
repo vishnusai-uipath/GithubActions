529492644488b1f54119837c648a074079ed62ed
vishnusai-uipath <104299909+vishnusai-uipath@users.noreply.github.com>
2025-03-19 13:40:31 +0530

    Update UiPathPack.ps1

--- scripts/UiPathPack.ps1
+++ scripts/UiPathPack.ps1
@@ -154,7 +154,7 @@ if($uipathCliFilePath -ne ""){
         $cliVersion = $SpecificCLIVersion;
     }
     else{
-        $cliVersion = "24.12.9166.24491"; #CLI Version (Script was tested on this latest version at the time)
+        $cliVersion = "24.10.9050.17872"; #CLI Version (Script was tested on this latest version at the time)
     }
     #Verifying UiPath CLI installation
     $uipathCLI = "$scriptPath\uipathcli\$cliVersion\tools\uipcli.exe"
